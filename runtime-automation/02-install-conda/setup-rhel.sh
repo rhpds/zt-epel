68eafe6d2a5d859504fb58b07f32a1e6c51aa7b9
Matthew Yee <myee@redhat.com>
2025-09-12 09:59:46 -0700

    Add EPEL installation confirmation image, enhance documentation for Conda installation, and introduce setup, solve, and validate scripts for RHEL 10.

---
+++ runtime-automation/02-install-conda/setup-rhel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Starting module called 02-install-conda" >> /tmp/progress.log
+
+sed -e '/^metalink=https:\/\/mirrors.fedoraproject.org\/metalink/ s/^/#/' \
+    -e '/^#baseurl=http:/ s/http/https/' \
+    -e '/^#baseurl=https:\/\/download.example/ s/^#//' \
+    -e '/^baseurl=https:\/\/download.example/ s_https://download.example_https://dl.fedoraproject.org_' \
+    -i /etc/yum.repos.d/epel*.repo
---
+++ runtime-automation/02-install-conda/validate-rhel.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "Validated module called 02-install-conda" >> /tmp/progress.log
